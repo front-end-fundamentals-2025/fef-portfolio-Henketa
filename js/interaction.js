bad86f193d4f82a5bedb9cfd6de12efde37a2fd3
Henketa <henrik6251@gmail.com>
2025-02-24 14:20:40 +0100

    more efficient js code
    -removed the individual lines of code in js, meant to change the colors of each line of text when in light mode.
    -added style color change to body element instead since it changes all the text color

--- js/interaction.js
+++ js/interaction.js
@@ -3,25 +3,12 @@ const lightMode = document.getElementById("Light-button");
 const darkMode = document.getElementById("Dark-button");
 
 const bodyElement = document.querySelector("body");
-const headerTextElement = document.getElementById("headerText");
-const aboutTextElement = document.getElementById("aboutText");
-const paragraphTextElement = document.getElementById("paragraphText");
-const likeTextElement = document.getElementById("likeText");
-const listTextElement = document.querySelector("li");
 
 lightMode.addEventListener("click", function (event) {
     bodyElement.style.backgroundColor = "#ffffff";
-    headerTextElement.style.color = "black";
-    aboutTextElement.style.color = "black";
-    paragraphTextElement.style.color = "black";
-    likeTextElement.style.color = "black";
-    listTextElement.style.color = "black";
+    bodyElement.style.color = "black";
 });
 darkMode.addEventListener("click", function (event) {
     bodyElement.style.backgroundColor = "#383838";
-    headerTextElement.style.color = "aliceblue";
-    aboutTextElement.style.color = "aliceblue";
-    paragraphTextElement.style.color = "aliceblue";
-    likeTextElement.style.color = "aliceblue";
-    listTextElement.style.color = "aliceblue";
+    bodyElement.style.color = "aliceblue";
 });
